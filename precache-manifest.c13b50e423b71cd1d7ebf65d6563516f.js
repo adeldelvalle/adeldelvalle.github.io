6645ce44ee2b2caf0fd56a9281a2b4dcb4a920b0
Adel del Valle <adel.delvalle@upr.edu>
2020-04-02 18:58:49 -0400

    Updates

---
+++ precache-manifest.c13b50e423b71cd1d7ebf65d6563516f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceeb538e5545e73b8c5daeabccdfc35c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744e5a24138a8acd24b1",
+    "url": "/static/css/main.9d3677e9.chunk.css"
+  },
+  {
+    "revision": "0fd04ecc5e161cfe82ee",
+    "url": "/static/js/2.4c4a4f61.chunk.js"
+  },
+  {
+    "revision": "4877ccfefea94478af51bdba4fdf7a71",
+    "url": "/static/js/2.4c4a4f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744e5a24138a8acd24b1",
+    "url": "/static/js/main.2196962b.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "53c162ac9dce41d8fb5010602e901087",
+    "url": "/static/media/content.53c162ac.jpg"
+  },
+  {
+    "revision": "4d15edf27b8e502ab551a50f386ef256",
+    "url": "/static/media/foto.4d15edf2.jpg"
+  },
+  {
+    "revision": "bae46ad21722b176c76f4752bfc4519d",
+    "url": "/static/media/geo.bae46ad2.jpg"
+  },
+  {
+    "revision": "db58dabcb3eb953d63a1cab130bc058f",
+    "url": "/static/media/headerbg.db58dabc.jpg"
+  }
+]);
